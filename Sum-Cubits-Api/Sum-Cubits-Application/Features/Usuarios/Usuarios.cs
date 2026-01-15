84947495613980d0b71e686d16ced308fc7d9ab4
Ivan Contreras <ivan.contreras@cablasociados.com>
2026-01-15 17:35:54 -0300

    refactor: actualizar propiedad Fecha_Baja a FechaBaja para consistencia en Usuarios y configuración de base de datos

--- Sum-Cubits-Api/Sum-Cubits-Application/Features/Usuarios/Usuarios.cs
+++ Sum-Cubits-Api/Sum-Cubits-Application/Features/Usuarios/Usuarios.cs
@@ -12,7 +12,7 @@ namespace Sum_Cubits_Application.Features.Usuarios
         public  string Email { get; set; }
         public string? Telefono { get; set; }
         public DateTime? FechaRegistro { get; set; }
-        public DateTime? Fecha_Baja { get; set; }
+        public DateTime? FechaBaja { get; set; }
         public  bool Activo { get; set; }
         public int? UsuarioBajaId { get; set; }
     }
